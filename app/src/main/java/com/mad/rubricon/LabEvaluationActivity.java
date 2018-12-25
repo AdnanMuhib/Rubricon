7e32b1ac682922d4a3402ea285d40baae558c5f0
Adnan <adnan.muhib@rocketmail.com>
2018-12-26 04:57:26 +0500

    Update, UI for Lab Evaluation

--- app/src/main/java/com/mad/rubricon/LabEvaluationActivity.java
+++ app/src/main/java/com/mad/rubricon/LabEvaluationActivity.java
@@ -1,7 +1,9 @@
 package com.mad.rubricon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class LabEvaluationActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class LabEvaluationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab_evaluation);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_home:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+
     }
 }
--- app/src/main/java/com/mad/rubricon/MainActivity.java
+++ app/src/main/java/com/mad/rubricon/MainActivity.java
@@ -3,7 +3,9 @@ package com.mad.rubricon;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void btnCourseCreationClicked(View view){
@@ -44,6 +47,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public  void btnLabReportingClicked(View view){
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_home:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
 
     }
 }
