11f09c781c622a1917d5f3a53145b425e47684ee
Adnan <adnan.muhib@rocketmail.com>
2018-12-27 09:19:47 +0500

    :ok_hand:Update: Add Logic to Move to Questions Activity from Selected Course Week

--- app/src/main/java/com/mad/rubricon/LabEvalSelectWeekActivity.java
+++ app/src/main/java/com/mad/rubricon/LabEvalSelectWeekActivity.java
@@ -1,8 +1,12 @@
 package com.mad.rubricon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,7 +27,19 @@ public class LabEvalSelectWeekActivity extends AppCompatActivity {
         courseWeeksList.add("Course Id" + courseId);
         ListView mCourseListView  = (ListView) findViewById(R.id.listViewCourseWeeks);
 
+        // when a week is clicked from the list
+        mCourseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = (String)parent.getItemAtPosition(position);
+                Log.i("DebugLog", value);
+
+                // code to go to next activity where Questions will be Displayed for Evaluation
+                GoToQuestionsActivity(value);
+            }
+        });
         ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, courseWeeksList);
+
         mCourseListView.setAdapter(adapter);
 
     }
@@ -42,6 +58,12 @@ public class LabEvalSelectWeekActivity extends AppCompatActivity {
         lst.add("Lab Week 5");
 
         return  lst;
+    }
+    public void GoToQuestionsActivity(String val){
+        //Intent intent = new Intent(this, MainActivity.class);
+        //intent.putExtra("CourseWeek", val);
+        Log.i("DebugLog", "Selected Course week is:  "+ val);
+        //startActivity(intent);
     }
 
 
