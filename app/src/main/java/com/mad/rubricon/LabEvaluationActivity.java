275831531cf04d8bdd689f5c644cfa42dc05e035
Adnan <adnan.muhib@rocketmail.com>
2018-12-27 07:47:21 +0500

    :ok_hand: Update: Lab Evaluation Main Activity to Use Course Custom List Adapter

--- app/src/main/java/com/mad/rubricon/LabEvaluationActivity.java
+++ app/src/main/java/com/mad/rubricon/LabEvaluationActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class LabEvaluationActivity extends AppCompatActivity {
 
@@ -11,6 +14,25 @@ public class LabEvaluationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab_evaluation);
+        ArrayList<Course> courses = getCoursesList();
+        CourseListCustomAdapter adapter = new CourseListCustomAdapter(courses, this);
+        ListView coursesListView = (ListView) findViewById(R.id.mCourseList);
+        coursesListView.setAdapter(adapter);
+    }
+    public ArrayList<Course> getCoursesList(){
+        // get list of Courses  from Database
+
+            // code goes here....
+
+        // Dummy Data for Testing
+        ArrayList<Course> crses = new ArrayList<Course>();
+        crses.add(new Course(1,"CS-125L", "Introduction to Computing"));
+        crses.add(new Course(2,"CS-112L", "Programming Fundamentals"));
+        crses.add(new Course(3,"CS-121L", "Assembly Programming"));
+        crses.add(new Course(4,"CS-123L", "Object Oriented Programming"));
+        crses.add(new Course(5,"CS-145L", "Data Structures"));
+
+        return crses;
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
