d57a3b79876e2d8fc6035918a7b43b360bd09570
Adnan <adnan.muhib@rocketmail.com>
2019-01-06 19:15:53 +0500

    :ok_hand: :sweat_smile: ADD: Pdf Report Viewer, Create PDF Report

--- app/src/main/java/com/mad/rubricon/LabReportActivity.java
+++ app/src/main/java/com/mad/rubricon/LabReportActivity.java
@@ -1,13 +1,22 @@
 package com.mad.rubricon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LabReportActivity extends AppCompatActivity {
-
+    String CourseId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab_report);
+        CourseId = getIntent().getStringExtra("crsId");
+    }
+
+    public  void OnGenerateReportClicked(View view){
+        Intent intent = new Intent(this, LabReportViewActivity.class);
+        intent.putExtra("crsId",CourseId);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/mad/rubricon/LabReportViewActivity.java
@@ -0,0 +1,290 @@
+package com.mad.rubricon;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
+import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.html.WebColors;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.shockwave.pdfium.PdfDocument;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.List;
+
+public class LabReportViewActivity extends Activity implements OnPageChangeListener,OnLoadCompleteListener {
+    private static final String TAG = LabReportViewActivity.class.getSimpleName();
+    public static final String SAMPLE_FILE = "report1.pdf";
+    PDFView pdfView;
+    Integer pageNumber = 0;
+    String pdfFileName;
+    String courseId;
+
+    private PdfPCell cell;
+    private String textAnswer;
+    private Image bgImage;
+    ListView list;
+    private String path;
+    private File dir;
+    private File file;
+
+    //use to set background color
+    BaseColor myColor = WebColors.getRGBColor("#9E9E9E");
+    BaseColor myColor1 = WebColors.getRGBColor("#757575");
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_report_view);
+
+        path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Rubricon/Reports";
+        String file_path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Rubricon/Reports/Report.pdf";
+        dir = new File(path);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        try {
+            createPDF();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+        courseId = getIntent().getStringExtra("crsId");
+
+        TextView txtTitle = (TextView) findViewById(R.id.textView_header);
+        txtTitle.setText("Course Title:" + courseId);
+        pdfView= (PDFView)findViewById(R.id.pdfView);
+        //displayFromAsset(SAMPLE_FILE);
+        displayFromFile(file_path);
+    }
+    private void displayFromFile(String filePath){
+        pdfView.fromFile(new File(filePath))
+                .defaultPage(pageNumber)
+                .enableSwipe(true)
+
+                .swipeHorizontal(true)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .load();
+    }
+    private void displayFromAsset(String assetFileName) {
+        pdfFileName = assetFileName;
+
+        pdfView.fromAsset(SAMPLE_FILE)
+                .defaultPage(pageNumber)
+                .enableSwipe(true)
+
+                .swipeHorizontal(true)
+                .onPageChange(this)
+                .enableAnnotationRendering(true)
+                .onLoad(this)
+                .scrollHandle(new DefaultScrollHandle(this))
+                .load();
+    }
+    @Override
+    public void loadComplete(int nbPages) {
+        PdfDocument.Meta meta = pdfView.getDocumentMeta();
+        printBookmarksTree(pdfView.getTableOfContents(), "-");
+    }
+
+    @Override
+    public void onPageChanged(int page, int pageCount) {
+        pageNumber = page;
+        setTitle(String.format("%s %s / %s", pdfFileName, page + 1, pageCount));
+    }
+    public void printBookmarksTree(List<PdfDocument.Bookmark> tree, String sep) {
+        for (PdfDocument.Bookmark b : tree) {
+
+            Log.e(TAG, String.format("%s %s, p %d", sep, b.getTitle(), b.getPageIdx()));
+
+            if (b.hasChildren()) {
+                printBookmarksTree(b.getChildren(), sep + "-");
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void createPDF() throws FileNotFoundException, DocumentException {
+
+        //create document file
+        Document doc = new Document();
+        try {
+
+            Log.e("PDFCreator", "PDF Path: " + path);
+            //SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyy");
+            file = new File(dir, "Report.pdf");
+            FileOutputStream fOut = new FileOutputStream(file);
+            PdfWriter writer = PdfWriter.getInstance(doc, fOut);
+
+            //open the document
+            doc.open();
+//create table
+            PdfPTable pt = new PdfPTable(3);
+            pt.setWidthPercentage(100);
+            float[] fl = new float[]{20, 45, 35};
+            pt.setWidths(fl);
+            cell = new PdfPCell();
+            cell.setBorder(Rectangle.NO_BORDER);
+
+            //set drawable in cell
+           // Drawable myImage = MainActivity.this.getResources().getDrawable(R.drawable.trinity);
+          //  Bitmap bitmap = ((BitmapDrawable) myImage).getBitmap();
+          //  ByteArrayOutputStream stream = new ByteArrayOutputStream();
+          //  bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+          //  byte[] bitmapdata = stream.toByteArray();
+            try {
+               // bgImage = Image.getInstance(bitmapdata);
+               // bgImage.setAbsolutePosition(330f, 642f);
+               // cell.addElement(bgImage);
+               // pt.addCell(cell);
+                cell = new PdfPCell();
+                cell.setBorder(Rectangle.NO_BORDER);
+                cell.addElement(new Paragraph("Lab Report"));
+
+                cell.addElement(new Paragraph(""));
+                cell.addElement(new Paragraph(""));
+                pt.addCell(cell);
+                cell = new PdfPCell(new Paragraph(""));
+                cell.setBorder(Rectangle.NO_BORDER);
+                pt.addCell(cell);
+
+                PdfPTable pTable = new PdfPTable(1);
+                pTable.setWidthPercentage(100);
+                cell = new PdfPCell();
+                cell.setColspan(1);
+                cell.addElement(pt);
+                pTable.addCell(cell);
+                PdfPTable table = new PdfPTable(6);
+
+                float[] columnWidth = new float[]{6, 30, 30, 20, 20, 30};
+                table.setWidths(columnWidth);
+
+
+                cell = new PdfPCell();
+
+
+                cell.setBackgroundColor(myColor);
+                cell.setColspan(6);
+                cell.addElement(pTable);
+                table.addCell(cell);
+                cell = new PdfPCell(new Phrase(" "));
+                cell.setColspan(6);
+                table.addCell(cell);
+                cell = new PdfPCell();
+                cell.setColspan(6);
+
+                cell.setBackgroundColor(myColor1);
+
+                cell = new PdfPCell(new Phrase("#"));
+                cell.setBackgroundColor(myColor1);
+                table.addCell(cell);
+                cell = new PdfPCell(new Phrase("Header 1"));
+                cell.setBackgroundColor(myColor1);
+                table.addCell(cell);
+                cell = new PdfPCell(new Phrase("Header 2"));
+                cell.setBackgroundColor(myColor1);
+                table.addCell(cell);
+                cell = new PdfPCell(new Phrase("Header 3"));
+                cell.setBackgroundColor(myColor1);
+                table.addCell(cell);
+                cell = new PdfPCell(new Phrase("Header 4"));
+                cell.setBackgroundColor(myColor1);
+                table.addCell(cell);
+                cell = new PdfPCell(new Phrase("Header 5"));
+                cell.setBackgroundColor(myColor1);
+                table.addCell(cell);
+
+                //table.setHeaderRows(3);
+                cell = new PdfPCell();
+                cell.setColspan(6);
+
+                for (int i = 1; i <= 10; i++) {
+                    table.addCell(String.valueOf(i));
+                    table.addCell("Header 1 row " + i);
+                    table.addCell("Header 2 row " + i);
+                    table.addCell("Header 3 row " + i);
+                    table.addCell("Header 4 row " + i);
+                    table.addCell("Header 5 row " + i);
+
+                }
+
+                PdfPTable ftable = new PdfPTable(6);
+                ftable.setWidthPercentage(100);
+                float[] columnWidthaa = new float[]{30, 10, 30, 10, 30, 10};
+                ftable.setWidths(columnWidthaa);
+                cell = new PdfPCell();
+                cell.setColspan(6);
+                cell.setBackgroundColor(myColor1);
+                cell = new PdfPCell(new Phrase("Total Number"));
+                cell.setBorder(Rectangle.NO_BORDER);
+                cell.setBackgroundColor(myColor1);
+                ftable.addCell(cell);
+                cell = new PdfPCell(new Phrase(""));
+                cell.setBorder(Rectangle.NO_BORDER);
+                cell.setBackgroundColor(myColor1);
+                ftable.addCell(cell);
+                cell = new PdfPCell(new Phrase(""));
+                cell.setBorder(Rectangle.NO_BORDER);
+                cell.setBackgroundColor(myColor1);
+                ftable.addCell(cell);
+                cell = new PdfPCell(new Phrase(""));
+                cell.setBorder(Rectangle.NO_BORDER);
+                cell.setBackgroundColor(myColor1);
+                ftable.addCell(cell);
+                cell = new PdfPCell(new Phrase(""));
+                cell.setBorder(Rectangle.NO_BORDER);
+                cell.setBackgroundColor(myColor1);
+                ftable.addCell(cell);
+                cell = new PdfPCell(new Phrase(""));
+                cell.setBorder(Rectangle.NO_BORDER);
+                cell.setBackgroundColor(myColor1);
+                ftable.addCell(cell);
+                cell = new PdfPCell(new Paragraph("Footer"));
+                cell.setColspan(6);
+                ftable.addCell(cell);
+                cell = new PdfPCell();
+                cell.setColspan(6);
+                cell.addElement(ftable);
+                table.addCell(cell);
+                doc.add(table);
+                Toast.makeText(getApplicationContext(), "created PDF", Toast.LENGTH_LONG).show();
+            } catch (DocumentException de) {
+                Log.e("PDFCreator", "DocumentException:" + de);
+            } finally {
+                doc.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
