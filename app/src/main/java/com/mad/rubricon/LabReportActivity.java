1c52d8589b14d8f3bfb9a2b98989c864f956d9cc
Hamza <hamza.afzaal@rndtek.co.uk>
2019-01-06 19:33:43 +0500

    UI update for generate report activity :ok_hand:

--- app/src/main/java/com/mad/rubricon/LabReportActivity.java
+++ app/src/main/java/com/mad/rubricon/LabReportActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class LabReportActivity extends AppCompatActivity {
     String CourseId;
@@ -11,6 +14,12 @@ public class LabReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lab_report);
+        Toolbar toolbar = findViewById(R.id.toolbar_lab_report_activity);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle("Lab Report");
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_arrow_back_white);
         CourseId = getIntent().getStringExtra("crsId");
     }
 
@@ -18,5 +27,15 @@ public class LabReportActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LabReportViewActivity.class);
         intent.putExtra("crsId",CourseId);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
